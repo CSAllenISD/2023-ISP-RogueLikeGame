553017397258cd98a467f3eb99e5e1029377c61e
aclassypotater <bowermanevan@gmail.com>
2023-02-18 01:27:43 -0600

    fireball anim

--- RogueLikeGame/Projectiles/testProjectile.gd
+++ RogueLikeGame/Projectiles/testProjectile.gd
@@ -1,22 +1,21 @@
 extends Node2D
 
 
-const LIFESPAN = 30 # amount of time until projectile dies
+const LIFESPAN = 2 # amount of time until projectile dies
 const SPEED = 100
 const DAMAGE = 10
 var DIRECTION = 0#angle
-var _life
 var _velocity
 var chara
 var main
 var EFFECT = load("res://RogueLikeGame/Effects/test_projectile_destroy.tscn")
+var anim_timer = .2
 # Called when the node enters the scene tree for the first time.
 
 var rng = RandomNumberGenerator.new()
 func _ready():
 	rng.randomize()
 	#DIRECTION = rng.randf_range(0, 360.0)
-	self._life = LIFESPAN
 	self.main = get_parent()
 	#self.rotation_degrees = DIRECTION
 	#self._velocity = Vector2(0,SPEED).rotated(DIRECTION*3.14/180)
@@ -24,13 +23,20 @@ func _ready():
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var time_alive = 0
 func _process(delta):
-	self._life -= delta 
-	if self._life <= 0:
+	time_alive += delta
+	if LIFESPAN - time_alive < 0:
 		destroy()
+	anim_timer -= delta	
+	if anim_timer <= 0:
+		anim_timer = .2
+		if $Sprite.flip_h:
+			$Sprite.flip_h = 0
+		else:
+			$Sprite.flip_h = 1
 	self.position[0] += _velocity[0]*delta
 	self.position[1] += _velocity[1]*delta
-	
 func destroy():
 	var effect = self.EFFECT.instance()
 	effect.position = self.position
