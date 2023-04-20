97793dc95fa77a348c66f96aa6cb2ef4b7aee7fa
Vereie <bobbybjr1717@gmail.com>
2023-04-20 15:13:20 -0500

    Fixed main scene
    Changed the main scene to GAME.tscn and changed the menu new game button to open GAME.tscn as well

--- RogueLikeGame/title_screen/buttons/MenuButton.gd
+++ RogueLikeGame/title_screen/buttons/MenuButton.gd
@@ -5,4 +5,4 @@ export(String) var scene_to_load
 
 
 func _on_New_Game_Button_pressed():
-	get_tree().change_scene("res://RogueLikeGame/Rooms/StartingRoom.tscn") 
+	get_tree().change_scene("res://GAME.tscn") 
